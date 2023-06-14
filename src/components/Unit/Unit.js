55b0e81b31f79607f1aba3707ef1a697def5730d
ijklop90 <ijklop1234@gmail.com>
2023-06-14 16:23:04 +0300

    123

--- src/components/Unit/Unit.js
+++ src/components/Unit/Unit.js
@@ -5,7 +5,7 @@ const Unit = (props) => {
     const {id, url, title} = props
     
     return (
-    <div className="fields col offset-s3 s6">
+    <div className="fields col offset-m3 m6 s12">
         <img id="post" className="post-img" alt={id} src={url}></img>
         <p>{title}</p>
     </div>
